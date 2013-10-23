2351276cee1191fdd6f2c55be651f187e9bb0954
Darryl Hein <dhein@xmmedia.com>
2013-10-23 00:28:33 -0600

    moving the order info to it's own view

--- views/cart/email/admin_order.php
+++ views/cart/email/admin_order.php
@@ -1,33 +1,5 @@
 <?php echo View::factory('cart/email/cart')
 	->set($kohana_view_data); ?>
 
-<div style="height: 15px;"></div>
-
-<table cellpadding="3" cellspacing="0" width="100%" border="0">
-	<tbody>
-		<tr>
-			<td valign="top" width="33%">
-				<strong>Shipping Address</strong><br>
-				<?php echo Cart::address_html($order->shipping_formatted()); ?>
-			</td>
-			<td valign="top" width="33%">
-			<?php if ( ! $order->same_as_shipping_flag) { ?>
-				<strong>Billing Contact</strong><br>
-				<?php echo Cart::address_html($order->billing_contact_formatted()); ?>
-				<strong>Billing Address</strong><br>
-				<?php echo Cart::address_html($order->billing_address_formatted()); ?>
-			<?php } else { ?>
-				<strong>Billing Information</strong><br>
-				<em>Same as shipping</em>
-			<?php } // if ?>
-			</td>
-			<td valign="top" width="33%">Paid with <?php echo HTML::chars($paid_with['type'] . ' ending in ' . $paid_with['last_4']); ?><br><br>
-				Order Number: <?php echo HTML::chars($order->order_num); ?></td>
-		</tr>
-	</tbody>
-</table>
-
-<?php if ( ! empty($order->order_note)) { ?>
-<p><strong>Notes</strong>
-	<br><?php echo nl2br(HTML::chars($order->order_note)); ?></p>
-<?php } // if ?>
+<?php echo View::factory('cart/email/order_info')
+	->set($kohana_view_data); ?>
---
+++ views/cart/email/order_info.php
@@ -0,0 +1,30 @@
+<div style="height: 15px;"></div>
+
+<table cellpadding="3" cellspacing="0" width="100%" border="0">
+	<tbody>
+		<tr>
+			<td valign="top" width="33%">
+				<strong>Shipping Address</strong><br>
+				<?php echo Cart::address_html($order->shipping_formatted()); ?>
+			</td>
+			<td valign="top" width="33%">
+			<?php if ( ! $order->same_as_shipping_flag) { ?>
+				<strong>Billing Contact</strong><br>
+				<?php echo Cart::address_html($order->billing_contact_formatted()); ?>
+				<strong>Billing Address</strong><br>
+				<?php echo Cart::address_html($order->billing_address_formatted()); ?>
+			<?php } else { ?>
+				<strong>Billing Information</strong><br>
+				<em>Same as shipping</em>
+			<?php } // if ?>
+			</td>
+			<td valign="top" width="33%">Paid with <?php echo HTML::chars($paid_with['type'] . ' ending in ' . $paid_with['last_4']); ?><br><br>
+				Order Number: <?php echo HTML::chars($order->order_num); ?></td>
+		</tr>
+	</tbody>
+</table>
+
+<?php if ( ! empty($order->order_note)) { ?>
+<p><strong>Notes</strong>
+	<br><?php echo nl2br(HTML::chars($order->order_note)); ?></p>
+<?php } // if ?>
