3f3a7e7220739673710aa13f6ea58f825d06b802
Darryl Hein <dhein@xmmedia.com>
2014-05-27 13:06:42 -0600

    comment

--- classes/Controller/XM/Cart.php
+++ classes/Controller/XM/Cart.php
@@ -438,6 +438,7 @@ class Controller_XM_Cart extends Controller_Public {
 
 		// run any additional custom verification of the order before checking out
 		$order = Cart::pre_checkout($order);
+		// will optionally load the address from the user's account
 		$order = Cart::load_user_address($order);
 
 		$order->calculate_totals()
