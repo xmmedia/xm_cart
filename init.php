0f9913f5ccb4a89aaaedbe4e587e8040c8bcfc64
Darryl Hein <dhein@xmmedia.com>
2014-05-30 17:17:54 -0600

    removing comments

--- init.php
+++ init.php
@@ -8,74 +8,35 @@ $transaction_status_ids = (array) Cart_Config::load('transaction_status_ids');
 
 // define the order status constants
 if ( ! defined('CART_ORDER_STATUS_NEW')) {
-	/**
-	*   CONST :: Order Status - New Order / Unpaid
-	*   @var    string
-	*/
 	define('CART_ORDER_STATUS_NEW', $order_status_ids['new']);
 }
 if ( ! defined('CART_ORDER_STATUS_SUBMITTED')) {
-	/**
-	*   CONST :: Order Status - Submitted / Waiting for Payment
-	*   @var    string
-	*/
 	define('CART_ORDER_STATUS_SUBMITTED', $order_status_ids['submitted']);
 }
 if ( ! defined('CART_ORDER_STATUS_PAYMENT')) {
-	/**
-	*   CONST :: Order Status - Payment in Progress
-	*   @var    string
-	*/
 	define('CART_ORDER_STATUS_PAYMENT', $order_status_ids['payment']);
 }
 if ( ! defined('CART_ORDER_STATUS_PAID')) {
-	/**
-	*   CONST :: Order Status - Paid
-	*   @var    string
-	*/
 	define('CART_ORDER_STATUS_PAID', $order_status_ids['paid']);
 }
 if ( ! defined('CART_ORDER_STATUS_RECEIVED')) {
-	/**
-	*   CONST :: Order Status - Received
-	*   @var    string
-	*/
 	define('CART_ORDER_STATUS_RECEIVED', $order_status_ids['received']);
 }
 if ( ! defined('CART_ORDER_STATUS_SHIPPED')) {
-	/**
-	*   CONST :: Order Status - Shipped
-	*   @var    string
-	*/
 	define('CART_ORDER_STATUS_SHIPPED', $order_status_ids['shipped']);
 }
 if ( ! defined('CART_ORDER_STATUS_REFUNDED')) {
-	/**
-	*   CONST :: Order Status - Refunded
-	*   @var    string
-	*/
 	define('CART_ORDER_STATUS_REFUNDED', $order_status_ids['refunded']);
 }
 if ( ! defined('CART_ORDER_STATUS_CANCELLED')) {
-	/**
-	*   CONST :: Order Status - Cancelled
-	*   @var    string
-	*/
 	define('CART_ORDER_STATUS_CANCELLED', $order_status_ids['cancelled']);
 }
 if ( ! defined('CART_ORDER_STATUS_EMPTIED')) {
-	/**
-	*   CONST :: Order Status - Emptied
-	*   @var    string
-	*/
 	define('CART_ORDER_STATUS_EMPTIED', $order_status_ids['cancelled']);
 }
 
+// define available payment processor list key
 if ( ! defined('PAYMENT_PROCESSOR_LIST')) {
-	/**
-	*   CONST :: The list of payment processors to use.
-	*   @var    string
-	*/
 	define('PAYMENT_PROCESSOR_LIST', 'default');
 }
 
