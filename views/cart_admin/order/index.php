ef810dee7630f6c66721ff422f00fa991feba7e3
Darryl Hein <dhein@xmmedia.com>
2016-01-27 14:24:41 -0700

    adding date filtering on the orders page

--- views/cart_admin/order/index.php
+++ views/cart_admin/order/index.php
@@ -4,7 +4,12 @@
 			<h1>Orders</h1>
 			<?php echo $form_open; ?>
 				<?php echo $order_filters_html['status']; ?>
+				<label for="order_filters_start_date">From</label>
+				<?php echo $order_filters_html['start_date']; ?>
+				<label for="order_filters_end_date">To</label>
+				<?php echo $order_filters_html['end_date']; ?>
 			</form>
+			<?php echo HTML::anchor(Route::get('cart_admin_order_export')->uri(), 'Export'); ?>
 
 			<ul class="list list_orders js_list">
 				<?php echo $order_html; ?>
